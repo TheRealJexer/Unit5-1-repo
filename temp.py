193e0ab515b2b1d0cab9e1afcbc00efe15fba442
Bob <bob@gmail.com>
2023-10-17 17:47:36 -0600

    First step complete

---
+++ temp.py
@@ -0,0 +1,5 @@
+# create a code that converts celcius to fahrenheit
+# the formula for celcius to fahrenheit is degree(celcius) * (9/5) + 32
+celcius = 25
+conversion_formula = celcius * (9/5) + 32
+print(f"25 degrees celcius is {int(conversion_formula)} degrees fahrenheit.")
