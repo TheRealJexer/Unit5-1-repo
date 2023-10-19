bb81f21c2949d4fdb8428f0bd5f37e6f90fe3d67
Bob <bob@gmail.com>
2023-10-19 16:00:04 -0600

    Now this is Temp3.py completed lol

--- temp3.py
+++ temp3.py
@@ -5,12 +5,14 @@ celcius = 25
 fahrenheit = 82
 
 def celcius_to_fahrenheit(celcius):
+    """This function converts the argument (celcius) degrees to fahrenheit"""
     conversion_formula = celcius * (9/5) + 32
-    return print(f"25 degress celcius is {int(conversion_formula)} degrees fahrenheit.")
+    return print(f"{celcius} degress celcius is {int(conversion_formula)} degrees fahrenheit")
 
 def fahrenheit_to_celcius(fahrenheit):
+    """This function converts the argument (fahrenhiet) degress to celcius"""
     conversion_formula1 = ((fahrenheit - 32) * 5) / 9
-    return print(f"82 degress fahrenheit is {int(conversion_formula1)} degrees celcius. ")
+    return print(f"{fahrenheit} degress fahrenheit is {int(conversion_formula1)} degrees celcius")
 
 temp_fahrenheit = celcius_to_fahrenheit(celcius)
 temp_celcius = fahrenheit_to_celcius(fahrenheit)
