df739f6d1d276fdb269c22c4563b63ff6bf134b0
Bob <bob@gmail.com>
2023-10-19 15:36:36 -0600

    Final Changes to Temp2.py

--- temp2.py
+++ temp2.py
@@ -3,9 +3,10 @@
 # create a function to run this code
 
 def celcius_to_fahranheit(n):
+    """This function takes an argument (n) and converts that number
+    to a fahrenheit degree from a (n) celcius degree"""
     # formula for celcius to fahranheit is degree(celcius) * (9/5) + 32
     return n * (9/5) + 32
     
-
 conversion = celcius_to_fahranheit(25)
 print(f"25 degrees celcius is {conversion} degrees fahranheit")
